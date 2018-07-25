e6b774e7dba40d2b2e02f0e69a3058b6f311d92a
Andre Franco <terceirizado.andrefranco@atlasproj.com-at-980252037528>
2018-07-25 17:31:04 -0300

    Seguindo o fluxo

---
+++ src/main/java/com/jms/ActivemqJmsApplication.java
@@ -0,0 +1,12 @@
+package com.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqJmsApplication.class, args);
+	}
+}
